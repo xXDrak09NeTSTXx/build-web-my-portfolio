1575b1bc9502e8f967c28cdf23c5048770bba8bf
xXDarK09NeTSTXx <mustofathalib0709@gmail.com>
2020-08-01 17:34:28 +0700

    menambah kan validation  untuk form di file script.js baris ke-58 sampai ke-78

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@ const menuToggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector("nav ul");
 
 menuToggle.addEventListener('click', function () {
-    nav.classList.toggle('siled');
+  nav.classList.toggle('siled');
 });
 
 
@@ -12,10 +12,10 @@ const link = document.querySelector(".list-nav");
 const removes = document.querySelectorAll(".link");
 
 link.addEventListener('click', function (l) {
-    removes.forEach(function (remove) {
-        remove.className = 'link';
-    })
-    l.target.classList.add("active-link");
+  removes.forEach(function (remove) {
+    remove.className = 'link';
+  })
+  l.target.classList.add("active-link");
 });
 
 
@@ -35,15 +35,15 @@ gambar[4] = "url('assets/img/jumbotron.jpg')";
 
 // Change Image
 function ubahGambar() {
-    document.getElementById("jumbotron").style.backgroundImage = gambar[i];
+  document.getElementById("jumbotron").style.backgroundImage = gambar[i];
 
-    if (i < gambar.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
+  if (i < gambar.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
 
-    setTimeout("ubahGambar()", time);
+  setTimeout("ubahGambar()", time);
 }
 
 window.onload = ubahGambar;
@@ -51,33 +51,34 @@ window.onload = ubahGambar;
 
 // menyimpan dari form kontak
 if (typeof (Storage) !== "undefined") {
-    // Browser mendukung sessionStorage/localStorage.
-    const kirim = document.querySelector("#kirim");
+  // Browser mendukung sessionStorage/localStorage.
+  const kirim = document.querySelector("#kirim");
 
-    kirim.addEventListener('click', function () {
-        const nama = document.getElementById("nama").value;
-        const email = document.getElementById("email").value;
-        const nomor = document.getElementById("nomor").value;
-        const pesan = document.getElementById("pesan").value;
-        if (nama == !null && email == !null && nomor == !null && pesan == !null) {
-            alert("data berhasil di simpan");
+  kirim.addEventListener('click', function () {
 
+    const nama = document.getElementById("nama").value;
+    const email = document.getElementById("email").value;
+    const nomor = document.getElementById("nomor").value;
+    const pesan = document.getElementById("pesan").value;
 
-            localStorage.setItem("nama", nama);
-            localStorage.setItem("email", email);
-            localStorage.setItem("nomor", nomor);
-            localStorage.setItem("pesan", pesan);
+    if (nama && email && nomor && pesan) {
+      localStorage.setItem("nama", nama);
+      localStorage.setItem("email", email);
+      localStorage.setItem("nomor", nomor);
+      localStorage.setItem("pesan", pesan);
 
-            localStorage.getItem("nama");
-            localStorage.getItem("email");
-            localStorage.getItem("nomor");
-            localStorage.getItem("pesan");
-        } else {
-            alert("tolong isi semua kolum");
-        }
-    });
+      localStorage.getItem("nama");
+      localStorage.getItem("email");
+      localStorage.getItem("nomor");
+      localStorage.getItem("pesan");
+
+      alert("data berhasil di simpan");
+      return;
+    }
+    return alert("semua kolum harus di isi");
+  });
 } else {
-    // Browser tidak mendukung sessionStorage/LocalStorage
-    alert("maaf borwser anda tidak mendukung sisitem kami");
+  // Browser tidak mendukung sessionStorage/LocalStorage
+  alert("maaf borwser anda tidak mendukung sisitem kami");
 }
 
